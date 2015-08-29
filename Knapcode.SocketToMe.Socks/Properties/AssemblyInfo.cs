f13fbb7d8f3ee533ee14d3f724961eea0bd9dbf6
Joel Verhagen <joel.verhagen@gmail.com>
2015-08-29 13:22:37 -0700

    Move Socks code to its own project

---
+++ Knapcode.SocketToMe.Socks/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.SocketToMe.Socks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Knapcode.SocketToMe.Socks")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0aa78d19-e02b-4600-ab98-c05c04dfc2fa")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
