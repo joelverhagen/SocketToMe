b462d3314c50ac6d61dcb3e49b146463a68a8b67
Joel Verhagen <joel.verhagen@gmail.com>
2015-09-06 14:05:06 -0700

    Add T4 template for async and synchronous reads in ByteStreamReader

---
+++ Knapcode.SocketToMe/Support/ByteStreamReader.Read.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Knapcode.SocketToMe.Support
+{
+    public partial class ByteStreamReader
+	{
+        public string ReadLine()
+        {
+            EnsureFirstRead();
+
+            if (_bufferSize == 0)
+            {
+                return null;
+            }
+
+            var lineStream = new MemoryStream();
+            int lineEndingPosition = 0;
+            bool lineFinished = false;
+            while (lineEndingPosition < _lineEndingBuffer.Length && _bufferSize > 0)
+            {
+                int endPosition;
+                for (endPosition = _position; endPosition < _bufferSize; endPosition++)
+                {
+                    if (_buffer[endPosition] == _lineEndingBuffer[lineEndingPosition])
+                    {
+                        lineEndingPosition++;
+                        if (lineEndingPosition == _lineEndingBuffer.Length)
+                        {
+                            endPosition++;
+                            lineFinished = true;
+                            break;
+                        }
+                    }
+                    else if (lineEndingPosition > 0)
+                    {
+                        lineEndingPosition = 0;
+                    }
+                }
+
+                lineStream.Write(_buffer, _position, endPosition - _position);
+                _position = endPosition;
+
+                if (endPosition == _bufferSize && !lineFinished)
+                {
+                    _bufferSize = _stream.Read(_buffer, 0, _buffer.Length);
+                    _position = 0;
+                }
+            }
+
+            var line = _encoding.GetString(lineStream.GetBuffer(), 0, (int) lineStream.Length);
+            if (!_preserveLineEndings && lineFinished)
+            {
+                line = line.Substring(0, line.Length - _lineEnding.Length);
+            }
+
+            return line;
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            int read = 0;
+            if (_bufferSize >= 0)
+            {
+                read = Math.Min(count, _bufferSize - _position);
+                Buffer.BlockCopy(_buffer, _position, buffer, offset, read);
+                count -= read;
+                offset += read;
+                _position += read;
+
+                if (_position == _bufferSize)
+                {
+                    _bufferSize = -1;
+                }
+            }
+
+            if (count != 0)
+            {
+                read += _stream.Read(buffer, offset, count);
+            }
+
+            return read;
+        }
+
+        private void EnsureFirstRead()
+        {
+            if (_bufferSize < 0)
+            {
+                _bufferSize = _stream.Read(_buffer, 0, _buffer.Length);
+            }
+        }
+
+        public async Task<string> ReadLineAsync()
+        {
+            await EnsureFirstReadAsync();
+
+            if (_bufferSize == 0)
+            {
+                return null;
+            }
+
+            var lineStream = new MemoryStream();
+            int lineEndingPosition = 0;
+            bool lineFinished = false;
+            while (lineEndingPosition < _lineEndingBuffer.Length && _bufferSize > 0)
+            {
+                int endPosition;
+                for (endPosition = _position; endPosition < _bufferSize; endPosition++)
+                {
+                    if (_buffer[endPosition] == _lineEndingBuffer[lineEndingPosition])
+                    {
+                        lineEndingPosition++;
+                        if (lineEndingPosition == _lineEndingBuffer.Length)
+                        {
+                            endPosition++;
+                            lineFinished = true;
+                            break;
+                        }
+                    }
+                    else if (lineEndingPosition > 0)
+                    {
+                        lineEndingPosition = 0;
+                    }
+                }
+
+                lineStream.Write(_buffer, _position, endPosition - _position);
+                _position = endPosition;
+
+                if (endPosition == _bufferSize && !lineFinished)
+                {
+                    _bufferSize = await _stream.ReadAsync(_buffer, 0, _buffer.Length);
+                    _position = 0;
+                }
+            }
+
+            var line = _encoding.GetString(lineStream.GetBuffer(), 0, (int) lineStream.Length);
+            if (!_preserveLineEndings && lineFinished)
+            {
+                line = line.Substring(0, line.Length - _lineEnding.Length);
+            }
+
+            return line;
+        }
+
+        public async Task<int> ReadAsync(byte[] buffer, int offset, int count)
+        {
+            int read = 0;
+            if (_bufferSize >= 0)
+            {
+                read = Math.Min(count, _bufferSize - _position);
+                Buffer.BlockCopy(_buffer, _position, buffer, offset, read);
+                count -= read;
+                offset += read;
+                _position += read;
+
+                if (_position == _bufferSize)
+                {
+                    _bufferSize = -1;
+                }
+            }
+
+            if (count != 0)
+            {
+                read += await _stream.ReadAsync(buffer, offset, count);
+            }
+
+            return read;
+        }
+
+        private async Task EnsureFirstReadAsync()
+        {
+            if (_bufferSize < 0)
+            {
+                _bufferSize = await _stream.ReadAsync(_buffer, 0, _buffer.Length);
+            }
+        }
+    }
+}
--- Knapcode.SocketToMe/Support/ByteStreamReader.cs
+++ Knapcode.SocketToMe/Support/ByteStreamReader.cs
@@ -1,11 +1,9 @@
-using System;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Knapcode.SocketToMe.Support
 {
-    public class ByteStreamReader
+    public partial class ByteStreamReader
     {
         private readonly Stream _stream;
         private readonly bool _preserveLineEndings;
@@ -33,91 +31,6 @@ namespace Knapcode.SocketToMe.Support
         public Stream GetRemainingStream()
         {
             return new PartiallyBufferedStream(_buffer, _position, _bufferSize - _position, _stream);
-        }
-
-        public async Task<string> ReadLineAsync()
-        {
-            await EnsureFirstReadAsync();
-
-            if (_bufferSize == 0)
-            {
-                return null;
-            }
-
-            var lineStream = new MemoryStream();
-            int lineEndingPosition = 0;
-            bool lineFinished = false;
-            while (lineEndingPosition < _lineEndingBuffer.Length && _bufferSize > 0)
-            {
-                int endPosition;
-                for (endPosition = _position; endPosition < _bufferSize; endPosition++)
-                {
-                    if (_buffer[endPosition] == _lineEndingBuffer[lineEndingPosition])
-                    {
-                        lineEndingPosition++;
-                        if (lineEndingPosition == _lineEndingBuffer.Length)
-                        {
-                            endPosition++;
-                            lineFinished = true;
-                            break;
-                        }
-                    }
-                    else if (lineEndingPosition > 0)
-                    {
-                        lineEndingPosition = 0;
-                    }
-                }
-
-                lineStream.Write(_buffer, _position, endPosition - _position);
-                _position = endPosition;
-
-                if (endPosition == _bufferSize && !lineFinished)
-                {
-                    _bufferSize = await _stream.ReadAsync(_buffer, 0, _buffer.Length);
-                    _position = 0;
-                }
-            }
-
-            var line = _encoding.GetString(lineStream.GetBuffer(), 0, (int) lineStream.Length);
-            if (!_preserveLineEndings && lineFinished)
-            {
-                line = line.Substring(0, line.Length - _lineEnding.Length);
-            }
-
-            return line;
-        }
-
-        public async Task<int> ReadAsync(byte[] buffer, int offset, int count)
-        {
-            int read = 0;
-            if (_bufferSize >= 0)
-            {
-                read = Math.Min(count, _bufferSize - _position);
-                Buffer.BlockCopy(_buffer, _position, buffer, offset, read);
-                count -= read;
-                offset += read;
-                _position += read;
-
-                if (_position == _bufferSize)
-                {
-                    _bufferSize = -1;
-                }
-            }
-
-            if (count != 0)
-            {
-                read += await _stream.ReadAsync(buffer, offset, count);
-            }
-
-            return read;
-        }
-
-        private async Task EnsureFirstReadAsync()
-        {
-            if (_bufferSize < 0)
-            {
-                _bufferSize = await _stream.ReadAsync(_buffer, 0, _buffer.Length);
-            }
         }
     }
 }
