58bc88636ee222788dcbcee91484ccae63fa220b
Joel Verhagen <joel.verhagen@gmail.com>
2015-09-06 00:25:07 -0700

    Add HttpHeaderCategories and PartiallyBufferedStream

---
+++ Knapcode.SocketToMe.Tests/Support/PartiallyBufferedStreamTests.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Knapcode.SocketToMe.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Knapcode.SocketToMe.Tests.Support
+{
+    [TestClass]
+    public class PartiallyBufferedStreamTests
+    {
+        [TestMethod]
+        public void It_Reads_Synchronously()
+        {
+            // ARRANGE
+            var buffer = Encoding.ASCII.GetBytes("foobarbaz");
+            var innerStream = new MemoryStream(Encoding.ASCII.GetBytes("FOOBAR"));
+            var stream = new PartiallyBufferedStream(buffer, 3, 3, innerStream);
+
+            // ACT
+            var content = new StreamReader(stream, Encoding.ASCII).ReadToEnd();
+
+            // ASSERT
+            content.Should().Be("barFOOBAR");
+        }
+
+        [TestMethod]
+        public async Task It_Reads_Asynchronously()
+        {
+            // ARRANGE
+            var buffer = Encoding.ASCII.GetBytes("foobarbaz");
+            var innerStream = new MemoryStream(Encoding.ASCII.GetBytes("FOOBAR"));
+            var stream = new PartiallyBufferedStream(buffer, 3, 3, innerStream);
+
+            // ACT
+            var content = await new StreamReader(stream, Encoding.ASCII).ReadToEndAsync();
+
+            // ASSERT
+            content.Should().Be("barFOOBAR");
+        }
+    }
+}
---
+++ Knapcode.SocketToMe/Http/HttpHeaderCategories.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace Knapcode.SocketToMe.Http
+{
+    public static class HttpHeaderCategories
+    {
+        private static readonly ISet<string> HttpGeneralHeaders;
+        private static readonly ISet<string> HttpRequestHeaders;
+        private static readonly ISet<string> HttpResponseHeaders;
+        private static readonly ISet<string> HttpContentHeaders;
+
+        static HttpHeaderCategories()
+        {
+            HttpGeneralHeaders = GetKnownHeaders(typeof (HttpContentHeaders).Assembly.GetType("System.Net.Http.Headers.HttpGeneralHeaders"));
+            HttpRequestHeaders = GetKnownHeaders(typeof (HttpRequestHeaders));
+            HttpResponseHeaders = GetKnownHeaders(typeof (HttpResponseHeaders));
+            HttpContentHeaders = GetKnownHeaders(typeof (HttpContentHeaders));
+        }
+
+        private static ISet<string> GetKnownHeaders(Type headerType)
+        {
+            var headers = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var addKnownHeaders = headerType.GetMethod("AddKnownHeaders", BindingFlags.Static | BindingFlags.NonPublic);
+            addKnownHeaders.Invoke(null, new object[] {headers});
+            return headers;
+        }
+
+        public static bool IsGeneralHeader(string header)
+        {
+            return HttpGeneralHeaders.Contains(header);
+        }
+
+        public static bool IsRequestHeader(string header)
+        {
+            return HttpRequestHeaders.Contains(header);
+        }
+
+        public static bool IsResponseHeader(string header)
+        {
+            return HttpResponseHeaders.Contains(header);
+        }
+
+        public static bool IsContentHeader(string header)
+        {
+            return HttpContentHeaders.Contains(header);
+        }
+    }
+}
