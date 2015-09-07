01b0ea102d009a8c96485ebd54188a16db3e2518
Joel Verhagen <joel.verhagen@gmail.com>
2015-09-07 14:20:34 -0700

    Use T4 template for ChainedStream

--- Knapcode.SocketToMe/Support/ChainedStream.cs
+++ Knapcode.SocketToMe/Support/ChainedStream.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Knapcode.SocketToMe.Support
 {
-    public class ChainedStream : Stream
+    public partial class ChainedStream : Stream
     {
         private readonly bool _disposeOnCompletion;
         private readonly IEnumerator<Stream> _streams;
@@ -59,72 +57,6 @@ namespace Knapcode.SocketToMe.Support
         public override void SetLength(long value)
         {
             throw new NotSupportedException();
-        }
-
-        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            // start the stream enumerator
-            if (!_started && !(_started = _streams.MoveNext()))
-            {
-                return 0;
-            }
-
-            // read the streams until the desired amount is returned
-            // TODO: support reading > 0 but < count
-            var totalRead = 0;
-            while (totalRead < count)
-            {
-                var read = await _streams.Current.ReadAsync(buffer, offset + totalRead, count - totalRead, cancellationToken);
-                if (read == 0)
-                {
-                    if (_disposeOnCompletion)
-                    {
-                        _streams.Current.Dispose();
-                    }
-
-                    if (!_streams.MoveNext())
-                    {
-                        return totalRead;
-                    }
-                }
-
-                totalRead += read;
-            }
-
-            return totalRead;
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            // start the stream enumerator
-            if ((!_started && !(_started = _streams.MoveNext())) || _finished)
-            {
-                return 0;
-            }
-
-            // read the streams until the desired amount is returned
-            var totalRead = 0;
-            while (totalRead < count)
-            {
-                var read = _streams.Current.Read(buffer, offset + totalRead, count - totalRead);
-                if (read == 0)
-                {
-                    if (_disposeOnCompletion)
-                    {
-                        _streams.Current.Dispose();
-                    }
-
-                    if (!_streams.MoveNext())
-                    {
-                        _finished = true;
-                        return totalRead;
-                    }
-                }
-
-                totalRead += read;
-            }
-
-            return totalRead;
         }
 
         public override void Write(byte[] buffer, int offset, int count)
