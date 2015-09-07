b27fcbd393c0bfaff71931ffb4c8c7caf08295f6
Joel Verhagen <joel.verhagen@gmail.com>
2015-09-07 12:15:27 -0700

    Improve the example code

--- Knapcode.SocketToMe.Sandbox/Program.cs
+++ Knapcode.SocketToMe.Sandbox/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using Knapcode.SocketToMe.Http;
 using Knapcode.SocketToMe.Socks;
 
@@ -10,16 +12,64 @@ namespace Knapcode.SocketToMe.Sandbox
     {
         private static void Main()
         {
-            var endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
-            var client = new Socks5Client();
+            Console.WriteLine("## HTTP ##");
+            HttpExample();
+            Console.WriteLine();
 
-            var socket = client.ConnectToServer(endpoint);
-            socket = client.ConnectToDestination(socket, "icanhazip.com", 443);
+            Console.WriteLine("## SOCKS ##");
+            SocksExample();
+            Console.WriteLine();
 
-            var httpClient = new HttpClient(new NetworkHandler(socket));
-            var response = httpClient.GetAsync("https://icanhazip.com/").Result;
+            Console.WriteLine("## HTTP and SOCKS ##");
+            HttpSocksExample();
+            Console.WriteLine();
+        }
+        
+        private static void HttpExample()
+        {
+            using (var httpClient = new HttpClient(new NetworkHandler()))
+            using (var response = httpClient.GetAsync("http://icanhazip.com/").Result)
+            {
+                Console.WriteLine("{0} {1}", (int) response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result.Trim());
+            }
+        }
 
-            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        private static void SocksExample()
+        {
+            // Tor support SOCKS 4, 4A, and 5
+            var socksEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
+            var socks5Client = new Socks5Client();
+            var socket = socks5Client.ConnectToServer(socksEndpoint);
+            socket = socks5Client.ConnectToDestination(socket, "icanhazip.com", 80);
+
+            using (var proxiedStream = new NetworkStream(socket))
+            using (var writer = new StreamWriter(proxiedStream))
+            using (var reader = new StreamReader(proxiedStream))
+            {
+                writer.WriteLine("GET / HTTP/1.1");
+                writer.WriteLine("Host: icanhazip.com");
+                writer.WriteLine();
+                writer.Flush();
+
+                Console.WriteLine(reader.ReadToEnd().Trim());
+            }
+        }
+
+        private static void HttpSocksExample()
+        {
+            // Tor support SOCKS 4, 4A, and 5
+            var socksEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
+            var socks5Client = new Socks5Client();
+            var socket = socks5Client.ConnectToServer(socksEndpoint);
+            socket = socks5Client.ConnectToDestination(socket, "icanhazip.com", 443);
+
+            using (var httpClient = new HttpClient(new NetworkHandler(socket)))
+            using (var response = httpClient.GetAsync("https://icanhazip.com/").Result)
+            {
+                Console.WriteLine("{0} {1}", (int)response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result.Trim());
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -16,35 +16,57 @@ Don't get too excited. You have to write your own HTTP stack if you want to send
 Install-Package Knapcode.SocketToMe
 ```
 
-## Example
+## Examples
 
 Talk to a website through Tor! Have [Tor Browser](https://www.torproject.org/download/download-easy.html.en) running at the same time to try this demo out. Port 9150 is the default port for Tor Browser.
 
+### HTTP Example
+
+```csharp
+using (var httpClient = new HttpClient(new NetworkHandler()))
+using (var response = httpClient.GetAsync("http://icanhazip.com/").Result)
+{
+    Console.WriteLine("{0} {1}", (int) response.StatusCode, response.ReasonPhrase);
+    Console.WriteLine(response.Content.ReadAsStringAsync().Result.Trim());
+}
+```
+
+### SOCKS Example
+
 ```csharp
 // Tor support SOCKS 4, 4A, and 5
-var endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
-
-// var client = new Socks4Client();
-var client = new Socks5Client();
-
-var socket = client.ConnectToServer(endpoint);
-socket = client.ConnectToDestination(socket, new IPEndPoint(IPAddress.Parse("104.238.136.31"), 443));
+var socksEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
+var socks5Client = new Socks5Client();
+var socket = socks5Client.ConnectToServer(socksEndpoint);
+socket = socks5Client.ConnectToDestination(socket, "icanhazip.com", 80);
 
 using (var proxiedStream = new NetworkStream(socket))
-using (var sslStream = new SslStream(proxiedStream))
+using (var writer = new StreamWriter(proxiedStream))
+using (var reader = new StreamReader(proxiedStream))
 {
-    sslStream.AuthenticateAsClient("icanhazip.com");
+    writer.WriteLine("GET / HTTP/1.1");
+    writer.WriteLine("Host: icanhazip.com");
+    writer.WriteLine();
+    writer.Flush();
 
-    using (var writer = new StreamWriter(sslStream))
-    using (var reader = new StreamReader(sslStream))
-    {
-        writer.WriteLine("GET / HTTP/1.1");
-        writer.WriteLine("Host: icanhazip.com");
-        writer.WriteLine();
-        writer.Flush();
+    Console.WriteLine(reader.ReadToEnd().Trim());
+}
+```
 
-        Console.WriteLine(reader.ReadToEnd());
-    }
+### HTTP + SOCKS
+
+```csharp
+// Tor support SOCKS 4, 4A, and 5
+var socksEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
+var socks5Client = new Socks5Client();
+var socket = socks5Client.ConnectToServer(socksEndpoint);
+socket = socks5Client.ConnectToDestination(socket, "icanhazip.com", 443);
+
+using (var httpClient = new HttpClient(new NetworkHandler(socket)))
+using (var response = httpClient.GetAsync("https://icanhazip.com/").Result)
+{
+    Console.WriteLine("{0} {1}", (int)response.StatusCode, response.ReasonPhrase);
+    Console.WriteLine(response.Content.ReadAsStringAsync().Result.Trim());
 }
 ```
 
