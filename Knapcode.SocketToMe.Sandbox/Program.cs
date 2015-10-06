1d787ab8559ffacef0bb797bc01593ec4f1b016f
Joel Verhagen <joel.verhagen@gmail.com>
2015-10-05 22:42:41 -0700

    Add minimal sandbox back

---
+++ Knapcode.SocketToMe.Sandbox/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using Knapcode.SocketToMe.Http;
+using Knapcode.SocketToMe.Socks;
+
+namespace Knapcode.SocketToMe.Sandbox
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("## HTTP ##");
+            HttpExample();
+            Console.WriteLine();
+
+            Console.WriteLine("## SOCKS ##");
+            SocksExample();
+            Console.WriteLine();
+
+            Console.WriteLine("## HTTP and SOCKS ##");
+            HttpSocksExample();
+            Console.WriteLine();
+        }
+        
+        private static void HttpExample()
+        {
+            using (var httpClient = new HttpClient(new NetworkHandler()))
+            using (var response = httpClient.GetAsync("http://icanhazip.com/").Result)
+            {
+                Console.WriteLine("{0} {1}", (int) response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result.Trim());
+            }
+        }
+
+        private static void SocksExample()
+        {
+            // Tor support SOCKS 4, 4A, and 5
+            var socksEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
+            var socks5Client = new Socks5Client();
+            var socket = socks5Client.ConnectToServer(socksEndpoint);
+            socket = socks5Client.ConnectToDestination(socket, "icanhazip.com", 80);
+
+            using (var proxiedStream = new NetworkStream(socket))
+            using (var writer = new StreamWriter(proxiedStream))
+            using (var reader = new StreamReader(proxiedStream))
+            {
+                writer.WriteLine("GET / HTTP/1.1");
+                writer.WriteLine("Host: icanhazip.com");
+                writer.WriteLine();
+                writer.Flush();
+
+                Console.WriteLine(reader.ReadToEnd().Trim());
+            }
+        }
+
+        private static void HttpSocksExample()
+        {
+            // Tor support SOCKS 4, 4A, and 5
+            var socksEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9150);
+            var socks5Client = new Socks5Client();
+            var socket = socks5Client.ConnectToServer(socksEndpoint);
+            socket = socks5Client.ConnectToDestination(socket, "icanhazip.com", 443);
+
+            using (var httpClient = new HttpClient(new NetworkHandler(socket)))
+            using (var response = httpClient.GetAsync("https://icanhazip.com/").Result)
+            {
+                Console.WriteLine("{0} {1}", (int)response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result.Trim());
+            }
+        }
+    }
+}
---
+++ Knapcode.SocketToMe.Sandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.SocketToMe.Sandbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Knapcode.SocketToMe.Sandbox")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9d7c588c-8aaf-4f40-9c33-5ed1bf8ee515")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
