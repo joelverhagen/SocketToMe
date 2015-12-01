182e7dcf131e01a86dbc6cf4f2a956333ed02da9
Joel Verhagen <jver@microsoft.com>
2015-11-30 18:50:47 -0800

    Add blob store interface and file system implementation

---
+++ Knapcode.SocketToMe/Http/Serializing/FileSystemBlobStore.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Knapcode.SocketToMe.Http
+{
+    public class FileSystemBlobStore : IBlobStore
+    {
+        private const int BufferSize = 4096;
+        private readonly string _directory;
+
+        public FileSystemBlobStore(string directory)
+        {
+            _directory = directory;
+        }
+
+        public Task<Stream> GetAsync(string key)
+        {
+            var path = GetPath(key);
+            try
+            {
+                var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, BufferSize, FileOptions.Asynchronous);
+                return Task.FromResult((Stream) fileStream);
+            }
+            catch (FileNotFoundException)
+            {
+                return null;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        public async Task SetAsync(string key, Stream stream)
+        {
+            var path = GetPath(key);
+            using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write, BufferSize, FileOptions.Asynchronous))
+            {
+                await stream.CopyToAsync(fileStream, BufferSize);
+            }
+        }
+
+        private string GetPath(string key)
+        {
+            return Path.Combine(_directory, key);
+        }
+    }
+}
---
+++ Knapcode.SocketToMe/Http/Serializing/IBlobStore.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Knapcode.SocketToMe.Http
+{
+    public interface IBlobStore
+    {
+        Task<Stream> GetAsync(string key);
+        Task SetAsync(string key, Stream stream);
+    }
+}
