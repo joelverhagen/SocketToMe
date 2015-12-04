bfa8b96dd78e4135f4cd92b6d0e66542a128f9be
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-04 11:21:27 -0800

    Clean up AssemblyInfo

--- Knapcode.SocketToMe.Tests/Properties/AssemblyInfo.cs
+++ Knapcode.SocketToMe.Tests/Properties/AssemblyInfo.cs
@@ -2,14 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Knapcode.SocketToMe.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Knapcode.SocketToMe.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e4d08f5c-364d-4b15-9d9a-91ab00cc9673")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
