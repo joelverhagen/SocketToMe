d5a61a3f029624160eecd43a99428fbcacd42e27
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-04 14:56:55 -0800

    Add test project for ProtocolBuffer project

---
+++ Knapcode.SocketToMe.Http.ProtocolBuffer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.SocketToMe.Http.ProtocolBuffer.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("447a2954-f5f2-42eb-9e22-c3005d5fe7f8")]
