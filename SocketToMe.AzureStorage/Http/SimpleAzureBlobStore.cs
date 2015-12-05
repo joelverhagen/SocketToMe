245c09ead87bf5a2e92aa9eed6091716eefa7f27
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-05 14:23:10 -0800

    Add SocketToMe.AzureStorage project and a simple blob store

---
+++ SocketToMe.AzureStorage/Http/SimpleAzureBlobStore.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Knapcode.SocketToMe.Http;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Knapcode.SocketToMe.AzureStorage.Http
+{
+    public class SimpleAzureBlobStore : IStore
+    {
+        private readonly CloudBlobContainer _container;
+
+        public SimpleAzureBlobStore(CloudBlobContainer container)
+        {
+            _container = container;
+        }
+
+        public async Task<Stream> GetAsync(string key, CancellationToken cancellationToken)
+        {
+            var reference = GetBlockBlobReference(key);
+            var exists = await reference.ExistsAsync(cancellationToken).ConfigureAwait(false);
+            if (!exists)
+            {
+                return null;
+            }
+
+            return await reference.OpenReadAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private CloudBlockBlob GetBlockBlobReference(string key)
+        {
+            return _container.GetBlockBlobReference(key);
+        }
+
+        public async Task SetAsync(string key, Stream stream, CancellationToken cancellationToken)
+        {
+            var reference = GetBlockBlobReference(key);
+            await reference.UploadFromStreamAsync(stream, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ SocketToMe.AzureStorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.SocketToMe.AzureStorage")]
+[assembly: ComVisible(false)]
+[assembly: Guid("284f9bf0-f5ed-433b-bc96-a74d2107cb44")]
