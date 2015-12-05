915f26226835296a69da16ba478128d3a2b1fa0c
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-05 15:09:17 -0800

    Add test project for AzureStorage project

---
+++ SocketToMe.AzureStorage.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Knapcode.SocketToMe.AzureStorage.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0558472f-bf52-48fd-aef1-7b0bd53d40d4")]
